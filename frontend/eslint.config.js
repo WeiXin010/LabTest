41565cccb1d0792d6334ed9f801a8b67d8a0e94a
Wei Xin <lwxin87@gmail.com>
2025-07-23 16:27:33 +0800

    eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,20 +1,30 @@
- import globals from "globals";
- import pluginJs from "@eslint/js";
- import pluginSecurity from "eslint-plugin-security";
- import pluginSecurityNode from "eslint-plugin-security-node";
- import pluginUnsantized from "eslint-plugin-no-unsanitized";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginSecurity from "eslint-plugin-security";
+import pluginSecurityNode from "eslint-plugin-security-node";
+import pluginUnsantized from "eslint-plugin-no-unsanitized";
+import pluginReact from "eslint-plugin-react";
+import parserBabel from "@babel/eslint-parser";
 
- /** @type {import('eslint').Linter.Config[]} */
- export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      parser: parserBabel,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+        requireConfigFile: false,
+      },
+      globals: globals.browser,
+    },
     plugins: {
+      react: pluginReact,
       security: pluginSecurity,
       "security-node": pluginSecurityNode,
-      "no-unsanitized": pluginUnsantized
-
+      "no-unsanitized": pluginUnsantized,
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
@@ -22,6 +32,8 @@
       ...pluginSecurityNode.configs.recommended.rules,
       ...pluginUnsantized.configs.recommended.rules,
       "security/detect-eval-with-expression": "error",
-    }
-  }
- ];
+      "react/react-in-jsx-scope": "off", // Not needed in React 17+
+      "react/jsx-uses-react": "off",     // Not needed in React 17+
+    },
+  },
+];
