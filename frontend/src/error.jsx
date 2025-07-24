67909f2a697ba9af86de9bc4e83c8a07e9d72dd9
WeiXin010 <145105915+WeiXin010@users.noreply.github.com>
2025-07-24 14:08:30 +0800

    Delete frontend directory

--- frontend/src/error.jsx
+++
@@ -1,5 +0,0 @@
-function ErrorComponent(props) {
-  console.log("Hello from console!"); // ❌ should trigger "no-console"
-  return <h1>{props.title}</h1>;
-}
-export default ErrorComponent;
--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-export default {
-  server: {
-    host: true, // or use '0.0.0.0'
-    port: 5173, // optional
-  }
-};
-
